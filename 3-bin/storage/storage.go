5b7e21f21dc0d3ee8bc5e2ee2b276e5321cf42f8
Igor Zemskov <igor@zemskoff.su>
2026-05-04 00:47:54 +0300

    feat: add JSON save/load and file read with .json validation

--- 3-bin/storage/storage.go
+++ 3-bin/storage/storage.go
@@ -1,1 +1,28 @@
 package storage
+
+import (
+	"bin-cli/bins"
+	"bin-cli/files"
+	"encoding/json"
+)
+
+func SaveBinList(list *bins.BinList, path string) error {
+	data, err := json.Marshal(list)
+	if err != nil {
+		return err
+	}
+	return files.WriteFile(path, data)
+}
+
+func LoadBinList(path string) (*bins.BinList, error) {
+	data, err := files.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	var list bins.BinList
+	err = json.Unmarshal(data, &list)
+	if err != nil {
+		return nil, err
+	}
+	return &list, nil
+}
